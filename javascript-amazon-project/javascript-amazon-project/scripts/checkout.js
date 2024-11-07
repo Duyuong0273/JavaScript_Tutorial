68e2db3336ff5c380360a5cd761b6f00f643cdf6
Hoangduy273 <duyuong0273@gmail.com>
2024-11-07 16:53:00 +0700

    finish javascript

--- javascript-amazon-project/javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project/scripts/checkout.js
@@ -2,23 +2,28 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
-// import "../data/cart-class.js";
-// import "../data/backend-practice.js";
+// import '../data/cart-class.js';
+// import '../data/backend-practice.js';
 
 async function loadPage() {
-  console.log("load page");
+  try {
+    // throw 'error1';
 
-  const value = await loadProductsFetch();
-  await new Promise((resolve) => {
-    loadProducts(() => {
-      resolve("valu3");
+    await loadProductsFetch();
+
+    const value = await new Promise((resolve, reject) => {
+      // throw 'error2';
+      loadCart(() => {
+        // reject('error3');
+        resolve("value3");
+      });
     });
-  });
+  } catch (error) {
+    console.log("Unexpected error. Please try again later.");
+  }
 
   renderOrderSummary();
   renderPaymentSummary();
-
-  return "value2";
 }
 loadPage();
 
@@ -26,34 +31,38 @@ loadPage();
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
-    loadProducts(() => {
+    loadCart(() => {
       resolve();
     });
-  }),
+  })
+
 ]).then((values) => {
   console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
+/*
 new Promise((resolve) => {
   loadProducts(() => {
-    resolve("value1");
+    resolve('value1');
   });
-})
-  .then((value) => {
-    console.log(value);
-    return new Promise((resolve) => {
-      loadCart(() => {
-        resolve();
-      });
+
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
     });
-  })
-  .then(() => {
-    renderOrderSummary();
-    renderPaymentSummary();
   });
-  */
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
 
 /*
 loadProducts(() => {
